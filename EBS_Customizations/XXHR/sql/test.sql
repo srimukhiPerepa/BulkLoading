105d414bf166af90097239c368bf48fcb5a173d2
Jay-ArBrouillard <brouillardjr@gmail.com>
2020-08-27 09:04:18 -0500

    Update test.sql

--- EBS_Customizations/XXHR/sql/test.sql
+++ EBS_Customizations/XXHR/sql/test.sql
@@ -1,9 +1,8 @@
 -- ---------------------------------------------------------------------------
 -- Script Name : test.sql
 -- Description : Testing if the SHOW ERRORS message is removed in the deployment
---               command on execution
+--               command on execution. During run time the deployment command
+--               will append SHOW ERRORS if EXIT is in the sql file.
 --
 -- ---------------------------------------------------------------------------
 
-EXIT
-/	
