a3d86eefbde43dc1b0a8f65b575eebdc722430f6
Jay-ArBrouillard <brouillardjr@gmail.com>
2020-08-27 08:59:49 -0500

    Create test.sql

---
+++ EBS_Customizations/XXHR/sql/test.sql
@@ -0,0 +1,9 @@
+-- ---------------------------------------------------------------------------
+-- Script Name : test.sql
+-- Description : Testing if the SHOW ERRORS message is removed in the deployment
+--               command on execution
+--
+-- ---------------------------------------------------------------------------
+
+EXIT
+/	
