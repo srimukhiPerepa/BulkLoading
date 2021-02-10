089a29435fedb0343de9e0ce8aac0a296fc836f6
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-02-10 14:50:07 -0600

    api_fingerprint

--- Terraform/oci_autonomousdb/provider.tf
+++ Terraform/oci_autonomousdb/provider.tf
@@ -1,6 +1,6 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
-variable "fingerprint" {}
+variable "api_fingerprint" {}
 variable "private_key_path" {}
 variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaanqjkbw76pcrpg24tcoroxgdi2ivtwcidkce4hjv2eksg2rgpo6da"}
 variable "region" {default="us-ashburn-1"}
