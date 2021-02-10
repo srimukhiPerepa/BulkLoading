20ce739444ff0b9a2af73c88159c1942f75daef9
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-02-10 15:24:31 -0600

    Change tf'

---
+++ Terraform/oci_vm/provider.tf
@@ -0,0 +1,11 @@
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "api_fingerprint" {}
+variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaaose23kfto7ito35t7mkowbjxgeqxshssjk4gejgfydqygbgfacja"}
+variable "region" {default="us-ashburn-1"}
+variable "tenancy_ocid" {}
+
+provider "oci" {
+  version              = ">= 3.14"
+  region               = "${var.region}"
+}
