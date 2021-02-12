3625e03e9aa59679ff99c8857a15d6a8b9bc3fa0
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-02-12 13:08:19 -0600

    Change to port 8500

--- Terraform/oci_autonomousdb/backend.tf
+++ Terraform/oci_autonomousdb/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "consul" {
     path    = "terraform/state/flex/oci/db/autonomous/demo1"
-	address = "teplt01.flexagon:22"
+	address = "teplt01.flexagon:8500"
     scheme  = "http"
     lock = true
   }
