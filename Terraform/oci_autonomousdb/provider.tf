ca43df20d3573468bccfceef5e7f4e148cb5d74b
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-02-12 13:53:05 -0600

    Added passphrase to provider.tf

--- Terraform/oci_autonomousdb/provider.tf
+++ Terraform/oci_autonomousdb/provider.tf
@@ -2,6 +2,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "api_fingerprint" {}
 variable "private_key_path" {}
+variable "pass_phrase" {}
 variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaanqjkbw76pcrpg24tcoroxgdi2ivtwcidkce4hjv2eksg2rgpo6da"}
 variable "region" {default="us-ashburn-1"}
 variable "autonomous_database_cpu_core_count" {default=1}
