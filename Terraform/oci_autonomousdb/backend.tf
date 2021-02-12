755008714175fc25387561883ae9c246e47cfaf1
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-02-12 13:05:55 -0600

    Add port number to address

--- Terraform/oci_autonomousdb/backend.tf
+++ Terraform/oci_autonomousdb/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "consul" {
     path    = "terraform/state/flex/oci/db/autonomous/demo1"
-	address = "teplt01.flexagon"
+	address = "teplt01.flexagon:22"
     scheme  = "http"
     lock = true
   }
