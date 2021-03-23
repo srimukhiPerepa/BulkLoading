cc8f7ef49d9e6593cbf6ec2df4790558c80cb9d6
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-03-23 16:20:58 -0500

    Add all

--- EBS_Customizations/XXHR/java/xxt/oracle/apps/ak/xxperson/schema/server/xxPersonDetailsEOImpl.java
+++ EBS_Customizations/XXHR/java/xxt/oracle/apps/ak/xxperson/schema/server/xxPersonDetailsEOImpl.java
@@ -17,7 +17,7 @@ import oracle.jbo.AttributeList;
 // Change 2
 // Change 4
 // Change 5
-// Change 6 7 8 9 10 11 12 13
+// Change 6 7 8 9 10 11 12 13 14
 
 public class xxPersonDetailsEOImpl extends OAEntityImpl 
 {
