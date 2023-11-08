f0b1cb67b7011d2fb22e489f5b9b5cc2fac9ed8e
Jay-ArBrouillard <brouillardjr@gmail.com>
2023-11-08 12:46:57 -0500

    Create BulkWorkflowPropertiesAndValues.groovy

---
+++ utilities/BulkWorkflowPropertiesAndValues.groovy
@@ -0,0 +1,31 @@
+import flexagon.ff.common.core.rest.FlexRESTClient;
+import flexagon.ff.common.core.rest.FlexRESTClientResponse;
+import flexagon.ff.common.core.utils.FlexJsonUtils;
+import flexagon.fd.model.integration.util.ApiException;
+ 
+import org.json.JSONObject;
+import org.json.JSONArray;
+  
+import java.util.HashMap;
+import java.util.Map;
+ 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+ 
+
+private FlexRESTClient getClient()
+{
+  FlexRESTClient restService = new FlexRESTClient();
+  restService.url("http://fdtlt86.flexagon.azure.com:8000").path("flexdeploy/rest/v1/workflows").basicauth("jayar", "Welcome1!").mediatype(MediaType.APPLICATION_JSON).setValidateResponse(true)
+  return restService;
+}
+
+public static void main(String[] args)
+{
+  FlexRESTClient restService = getClient()
+  FlexRESTClientResponse response = restService.get();
+           
+  JSONObject jsonResponse = FlexJsonUtils.getJSON(response.getResponseString());
+  println jsonResponse
+}
