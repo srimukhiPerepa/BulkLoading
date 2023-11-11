1a22179e0e08a2590baf3bc54dec680bc7f6e2c9
jbrouillard <jay-ar.brouillard@flexagon.com>
2023-11-11 01:33:29 -0500

    TGThread

---
+++ utilities/threads/WFThread.java
@@ -0,0 +1,266 @@
+package threads;
+
+import requests.WorkflowAPI;
+
+import pojo.PropertyDefinitionPojo;
+
+import threads.*;
+
+import flexagon.ff.common.core.exceptions.FlexCheckedException;
+import flexagon.ff.common.core.utils.FlexCommonUtils;
+import flexagon.ff.common.core.utils.FlexFileUtils;
+ 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.logging.*;
+
+import java.io.*;
+
+public class WFThread extends Thread
+{
+  private final String CLZ_NAM = WFThread.class.getName();
+  private final Logger LOGGER = Logger.getGlobal();
+
+  // in
+  private WorkflowAPI wfAPI;
+  private String workflowName;
+  private String workflowSource;
+  private String csvFilePath;
+
+  public WFThread(WorkflowAPI wfAPI, String workflowName, String workflowSource, String csvFilePath)
+  {
+    this.wfAPI = wfAPI;
+    this.workflowName = workflowName;
+    this.workflowSource = workflowSource;
+    this.csvFilePath = csvFilePath;
+  }
+
+  public void run()
+  {
+    try
+    {
+      JSONArray workflowsArray = wfAPI.findWorkflowByName(workflowName);
+      JSONObject workflowObject = validateWorkflowArray(workflowsArray);
+      workflowObject.put("sourceCode", workflowSource); // this is required for update workflow and get workflow does not return the value
+      workflowObject.put("sourceCodeURL", "dummy"); // this is required or validation will fail - sourceCodeURL is not actually used in backend
+
+      JSONArray workflowPropertiesJSONArray = workflowObject.getJSONArray("properties");
+      List<PropertyDefinitionPojo> existingWorkflowProperties = PropertyDefinitionPojo.convertObjectsToPropertyDefinition(workflowPropertiesJSONArray);
+
+      File csv = new File(csvFilePath);
+      List<String> lines = FlexFileUtils.read(csv);
+      List<PropertyDefinitionPojo> incomingWorkflowProperties = readAndProcessCSV(lines);
+      List<PropertyDefinitionPojo> mergedWorkflowProperties = mergeWorkflowProperties(existingWorkflowProperties, incomingWorkflowProperties);
+      writeWorkflowPropertiesToWorkflowObject(workflowObject, mergedWorkflowProperties);
+
+      String workflowId = workflowObject.get("workflowId").toString();
+      wfAPI.updateWorkflowById(workflowId, workflowObject.toString());
+    }
+    catch (FlexCheckedException fce)
+    {
+      throw new RuntimeException(fce);
+    }
+  }
+
+    /**
+   * Validates pJsonArray contains only one JSONObject and return the JSONObject
+   * pJsonArray - Array of JSONObject containing Workflow Definitions
+   */
+  private JSONObject validateWorkflowArray(JSONArray pJsonArray)
+    throws FlexCheckedException
+  {
+    final String methodName = "validateWorkflowArray";
+    LOGGER.entering(CLZ_NAM, methodName, pJsonArray);
+
+    if (pJsonArray.length() == 0)
+    {
+      throw new FlexCheckedException("No workflow(s) found with name " + WORKFLOW_NAME);
+    }
+
+    if (pJsonArray.length() > 1)
+    {
+      throw new FlexCheckedException("More than one workflow found with name " + WORKFLOW_NAME + ". WORKFLOW_NAME must be unique.");
+    }
+
+    JSONObject wfObject = pJsonArray.getJSONObject(0);
+
+    LOGGER.exiting(CLZ_NAM, methodName, wfObject);
+    return wfObject;
+  }
+
+  private List<PropertyDefinitionPojo> readAndProcessCSV(List<String> pLines)
+    throws FlexCheckedException
+  {
+    final String methodName = "readAndProcessCSV";
+    LOGGER.entering(CLZ_NAM, methodName, pLines);
+
+    List<PropertyDefinitionPojo> results = new ArrayList<>();
+    List<String> errors = new ArrayList<>();
+
+    String[] headers = pLines.get(0).split(",");
+    for (int i = 15; i < headers.length; i++)
+    {
+      String environmentCode = headers[i];
+      validateEnvironmentCode(environmentCode, errors);
+      targetEnvironmentCodes.add(environmentCode);
+    }
+
+    validateEnvironmentsMappedToTargetGroup(errors);
+
+    if (errors.size() > 0)
+    {
+      throw new FlexCheckedException(errors.toString());
+    }
+    LOGGER.finest("environmentCodeToEnvironmentId map: " + environmentCodeToEnvironmentId);
+
+    int numEnvironments = targetEnvironmentCodes.size();
+    int numLines = pLines.size();
+    for (int i = 1; i < numLines; i++)
+    {
+      String line = pLines.get(i);
+      String[] tokens = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+      String code = tokens[0];
+      String displayName = tokens[1];
+      String propertyScope = tokens[2];
+      String isRequired = tokens[3];
+      String dataType = tokens[4];
+      String isEncrypted = tokens[5];
+      String displayRows = tokens[6];
+      String displayColumns = tokens[7];
+      String listData = tokens[8];
+      String subDataType = tokens[9];
+      String isDefaultValueExpression = tokens[10];
+      String isMultiselect = tokens[11];
+      String description = tokens[12];
+      String isActive = tokens[13];
+      String defaultValue = tokens[14];
+
+      PropertyDefinitionPojo pojo = new PropertyDefinitionPojo();
+      pojo.setIsEncrypted(Boolean.valueOf(isEncrypted));
+      pojo.setIsRequired(Boolean.valueOf(isRequired));
+      pojo.setIsDefaultValueExpression(isDefaultValueExpression != null ? Boolean.valueOf(isDefaultValueExpression) : false);
+      pojo.setIsMultiselect(isMultiselect != null ? Boolean.valueOf(isMultiselect) : false);
+      pojo.setIsActive(isActive != null ? Boolean.valueOf(isActive) : true);
+      pojo.setDataType(dataType);
+      pojo.setScope(propertyScope);
+      pojo.setName(code);
+      pojo.setDisplayRows(FlexCommonUtils.isNotEmpty(displayRows) ? Integer.parseInt(displayRows.toString()) : null);
+      pojo.setDisplayColumns(FlexCommonUtils.isNotEmpty(displayColumns) ? Integer.parseInt(displayColumns.toString()) : null);
+      pojo.setListData(FlexCommonUtils.isNotEmpty(listData) ? Arrays.asList(listData.toString().trim().split(",")) : null);
+      pojo.setSubDataType(FlexCommonUtils.isNotEmpty(subDataType) ? subDataType.toString() : null);
+      pojo.setDisplayName(FlexCommonUtils.isNotEmpty(displayName) ? displayName.toString() : null);
+      pojo.setDescription(FlexCommonUtils.isNotEmpty(description) ? description.toString() : null);
+      pojo.setDefaultValue(FlexCommonUtils.isNotEmpty(defaultValue) ? defaultValue.toString() : null);
+
+      results.add(pojo);
+
+      if ("ENVINST".equals(propertyScope))
+      {
+        // will only have target property values if scope is ENVINST
+        for (int j = 0; j < numEnvironments; j++)
+        {
+          String key = code + targetEnvironmentCodes.get(j);
+          String value = tokens[j+15]; //important to add 15 here
+          codeToValue.put(key, value);
+        }
+      }
+
+      if (FlexCommonUtils.isEmpty(code))
+      {
+        errors.add("Line " + i + " is missing CODE");
+      }
+
+      if (FlexCommonUtils.isEmpty(displayName))
+      {
+        errors.add("Line " + i + " is missing DISPLAY_NAME");
+      }
+
+      if (FlexCommonUtils.isEmpty(propertyScope) || !(propertyScope.equals("ENVINST") || propertyScope.equals("PROJECT")))
+      {
+        errors.add("Line " + i + " PROPERTY_SCOPE must be ENVINST or PROJECT");
+      }
+
+      if (FlexCommonUtils.isEmpty(isRequired))
+      {
+        errors.add("Line " + i + " is missing REQUIRED");
+      }
+
+      if (FlexCommonUtils.isEmpty(dataType))
+      {
+        errors.add("Line " + i + " is missing DATA_TYPE");
+      }
+
+      if (FlexCommonUtils.isNotEmpty(subDataType) && !(subDataType.equals("DIRECTORY") || subDataType.equals("JDBCURL") || subDataType.equals("URL")))
+      {
+        errors.add("Line " + i + " SUB_DATA_TYPE must be DIRECTORY, JDBCURL or URL");
+      }
+
+      if (FlexCommonUtils.isEmpty(isEncrypted))
+      {
+        errors.add("Line " + i + " is missing ENCRYPTED");
+      }
+    }
+    LOGGER.finest("codeToValue mapping: " + codeToValue);
+
+    if (errors.size() > 0)
+    {
+      throw new FlexCheckedException(errors.toString());
+    }
+
+    LOGGER.exiting(CLZ_NAM, methodName, results.size());
+    return results;
+  }
+
+  private void validateEnvironmentCode(String pEnvironmentCode, List<String> pErrorList)
+    throws FlexCheckedException
+  {
+    final String methodName = "validateEnvironmentCode";
+    LOGGER.entering(CLZ_NAM, methodName, pEnvironmentCode);
+
+    LOGGER.info("Validating environment with code: " + pEnvironmentCode);
+    JSONArray result = envAPI.findEnvironmentByCode(pEnvironmentCode);
+    if (result.length() == 0)
+    {
+      // fail
+      pErrorList.add("Environment was not found with environment code " + pEnvironmentCode + ". Fix header in CSV file");
+    }
+    else
+    {
+      // success - environment exists
+      String environmentId = result.getJSONObject(0).get("environmentId").toString();
+      environmentCodeToEnvironmentId.put(pEnvironmentCode, environmentId);
+    }   
+
+    LOGGER.exiting(CLZ_NAM, methodName);
+  }
+
+  private void validateEnvironmentsMappedToTargetGroup(List<String> pErrorList)
+    throws FlexCheckedException
+  {
+    final String methodName = "validateEnviromentsMappedToTargetGroup";
+    LOGGER.entering(CLZ_NAM, methodName);
+
+    JSONObject targetGroupObject = tAPI.getTargetGroupById(targetGroupId);
+    JSONArray targetsArray = targetGroupObject.getJSONArray("targets");
+    // convert targetsArray to a List so we can use it with Streaming API
+    List<JSONObject> converted = IntStream.range(0, targetsArray.length())
+                                  .mapToObj(i -> targetsArray.getJSONObject(i))
+                                  .collect(Collectors.toList());
+    for (String environmentCode : targetEnvironmentCodes)
+    {
+      String environmentId = environmentCodeToEnvironmentId.get(environmentCode);
+      boolean isMapped = converted.stream().anyMatch(json -> json.get("environmentId").toString().equals(environmentId));
+      if (!isMapped)
+      {
+        pErrorList.add("Environment " + environmentCode + " is not mapped to target group " + TARGET_GROUP_CODE + " but referenced in CSV file header. Change header in CSV file or map environment to target group");
+      }
+    }
+
+    LOGGER.exiting(CLZ_NAM, methodName);
+  }
+}
--- utilities/workflow/BulkWorkflowPropertiesAndValues.java
+++ utilities/workflow/BulkWorkflowPropertiesAndValues.java
@@ -1,6 +1,5 @@
 package workflow;
 
-import requests.GetTargetGroupByCode;
 import requests.WorkflowAPI;
 import requests.CredentialAPI;
 import requests.EnvironmentAPI;
@@ -13,14 +12,10 @@ import threads.*;
 
 import flexagon.ff.common.core.exceptions.FlexCheckedException;
 import flexagon.ff.common.core.logging.FlexLogger;
-import flexagon.ff.common.core.rest.FlexRESTClientResponse;
-import flexagon.ff.common.core.utils.FlexCommonUtils;
-import flexagon.ff.common.core.utils.FlexFileUtils;
  
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -88,41 +83,23 @@ public class BulkWorkflowPropertiesAndValues
     System.out.println("//////////////////////////////////////////////////PREREQUISITE DATA///////////////////////////////////////////////////////////////////////////////////////");
     System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
 
-    TGThread tgt = new TGThread(tAPI, TARGET_GROUP_CODE);
+    WFThread wf = new WFThread(wfAPI, WORKFLOW_NAME, WORKFLOW_SOURCE, INPUT_CSV_FILE_PATH);
+    TGThread tg = new TGThread(tAPI, TARGET_GROUP_CODE);
     CSThread cs = new CSThread(credAPI);
-    tgt.start();
+    wf.start();
+    tg.start();
     cs.start();
-    tgt.join();
+    tg.join();
     cs.join();
+    wf.join();
 
-    targetGroupId = tgt.targetGroupId;
+    targetGroupId = tg.targetGroupId;
     localCredStoreId = cs.localCredStoreId;
     localCredStoreInputDefId = cs.localCredStoreInputDefId;
 
     LOGGER.fine("Target Group Id: " + targetGroupId);
     LOGGER.fine("Local Credential Store Id: " + localCredStoreId);
     LOGGER.fine("Local Credential Store Secret Text Definition Id: " + localCredStoreInputDefId);
-
-    System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
-    System.out.println("//////////////////////////////////////////////////CREATE/UPDATE WORKFLOW_PROPERTIES///////////////////////////////////////////////////////////////////////");
-    System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
-    
-    JSONArray workflowsArray = wfAPI.findWorkflowByName(WORKFLOW_NAME);
-    JSONObject workflowObject = validateWorkflowArray(workflowsArray);
-    workflowObject.put("sourceCode", WORKFLOW_SOURCE); // this is required for update workflow and get workflow does not return the value
-    workflowObject.put("sourceCodeURL", "dummy"); // this is required or validation will fail - sourceCodeURL is not actually used in backend
-
-    JSONArray workflowPropertiesJSONArray = workflowObject.getJSONArray("properties");
-    List<PropertyDefinitionPojo> existingWorkflowProperties = PropertyDefinitionPojo.convertObjectsToPropertyDefinition(workflowPropertiesJSONArray);
-
-    File csv = new File(INPUT_CSV_FILE_PATH);
-    List<String> lines = FlexFileUtils.read(csv);
-    List<PropertyDefinitionPojo> incomingWorkflowProperties = readAndProcessCSV(lines);
-    List<PropertyDefinitionPojo> mergedWorkflowProperties = mergeWorkflowProperties(existingWorkflowProperties, incomingWorkflowProperties);
-    writeWorkflowPropertiesToWorkflowObject(workflowObject, mergedWorkflowProperties);
-
-    String workflowId = workflowObject.get("workflowId").toString();
-    wfAPI.updateWorkflowById(workflowId, workflowObject.toString());
 
     System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
     System.out.println("///////////////////////////////////////////////////////CREATE/UPDATE CREDENTIALS//////////////////////////////////////////////////////////////////////////");
@@ -318,202 +295,5 @@ public class BulkWorkflowPropertiesAndValues
     JSONObject wfObject = pJsonArray.getJSONObject(0);
     LOGGER.exiting(CLZ_NAM, methodName, wfObject);
     return wfObject;
-  }
-
-  /**
-   * Validates pJsonArray contains only one JSONObject and return the JSONObject
-   * pJsonArray - Array of JSONObject containing Workflow Definitions
-   */
-  private static JSONObject validateWorkflowArray(JSONArray pJsonArray)
-    throws FlexCheckedException
-  {
-    final String methodName = "validateWorkflowArray";
-    LOGGER.entering(CLZ_NAM, methodName, pJsonArray);
-
-    if (pJsonArray.length() == 0)
-    {
-      throw new FlexCheckedException("No workflow(s) found with name " + WORKFLOW_NAME);
-    }
-
-    if (pJsonArray.length() > 1)
-    {
-      throw new FlexCheckedException("More than one workflow found with name " + WORKFLOW_NAME + ". WORKFLOW_NAME must be unique.");
-    }
-
-    JSONObject wfObject = pJsonArray.getJSONObject(0);
-
-    LOGGER.exiting(CLZ_NAM, methodName, wfObject);
-    return wfObject;
-  }
-
-  private static void validateEnvironmentCode(String pEnvironmentCode, List<String> pErrorList)
-    throws FlexCheckedException
-  {
-    final String methodName = "validateEnvironmentCode";
-    LOGGER.entering(CLZ_NAM, methodName, pEnvironmentCode);
-
-    LOGGER.info("Validating environment with code: " + pEnvironmentCode);
-    JSONArray result = envAPI.findEnvironmentByCode(pEnvironmentCode);
-    if (result.length() == 0)
-    {
-      // fail
-      pErrorList.add("Environment was not found with environment code " + pEnvironmentCode + ". Fix header in CSV file");
-    }
-    else
-    {
-      // success - environment exists
-      String environmentId = result.getJSONObject(0).get("environmentId").toString();
-      environmentCodeToEnvironmentId.put(pEnvironmentCode, environmentId);
-    }   
-
-    LOGGER.exiting(CLZ_NAM, methodName);
-  }
-
-  private static void validateEnvironmentsMappedToTargetGroup(List<String> pErrorList)
-    throws FlexCheckedException
-  {
-    final String methodName = "validateEnviromentsMappedToTargetGroup";
-    LOGGER.entering(CLZ_NAM, methodName);
-
-    JSONObject targetGroupObject = tAPI.getTargetGroupById(targetGroupId);
-    JSONArray targetsArray = targetGroupObject.getJSONArray("targets");
-    // convert targetsArray to a List so we can use it with Streaming API
-    List<JSONObject> converted = IntStream.range(0, targetsArray.length())
-                                  .mapToObj(i -> targetsArray.getJSONObject(i))
-                                  .collect(Collectors.toList());
-    for (String environmentCode : targetEnvironmentCodes)
-    {
-      String environmentId = environmentCodeToEnvironmentId.get(environmentCode);
-      boolean isMapped = converted.stream().anyMatch(json -> json.get("environmentId").toString().equals(environmentId));
-      if (!isMapped)
-      {
-        pErrorList.add("Environment " + environmentCode + " is not mapped to target group " + TARGET_GROUP_CODE + " but referenced in CSV file header. Change header in CSV file or map environment to target group");
-      }
-    }
-
-    LOGGER.exiting(CLZ_NAM, methodName);
-  }
-
-  private static List<PropertyDefinitionPojo> readAndProcessCSV(List<String> pLines)
-    throws FlexCheckedException
-  {
-    final String methodName = "readAndProcessCSV";
-    LOGGER.entering(CLZ_NAM, methodName, pLines);
-
-    List<PropertyDefinitionPojo> results = new ArrayList<>();
-    List<String> errors = new ArrayList<>();
-
-    String[] headers = pLines.get(0).split(",");
-    for (int i = 15; i < headers.length; i++)
-    {
-      String environmentCode = headers[i];
-      validateEnvironmentCode(environmentCode, errors);
-      targetEnvironmentCodes.add(environmentCode);
-    }
-
-    validateEnvironmentsMappedToTargetGroup(errors);
-
-    if (errors.size() > 0)
-    {
-      throw new FlexCheckedException(errors.toString());
-    }
-    LOGGER.finest("environmentCodeToEnvironmentId map: " + environmentCodeToEnvironmentId);
-
-    int numEnvironments = targetEnvironmentCodes.size();
-    int numLines = pLines.size();
-    for (int i = 1; i < numLines; i++)
-    {
-      String line = pLines.get(i);
-      String[] tokens = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-      String code = tokens[0];
-      String displayName = tokens[1];
-      String propertyScope = tokens[2];
-      String isRequired = tokens[3];
-      String dataType = tokens[4];
-      String isEncrypted = tokens[5];
-      String displayRows = tokens[6];
-      String displayColumns = tokens[7];
-      String listData = tokens[8];
-      String subDataType = tokens[9];
-      String isDefaultValueExpression = tokens[10];
-      String isMultiselect = tokens[11];
-      String description = tokens[12];
-      String isActive = tokens[13];
-      String defaultValue = tokens[14];
-
-      PropertyDefinitionPojo pojo = new PropertyDefinitionPojo();
-      pojo.setIsEncrypted(Boolean.valueOf(isEncrypted));
-      pojo.setIsRequired(Boolean.valueOf(isRequired));
-      pojo.setIsDefaultValueExpression(isDefaultValueExpression != null ? Boolean.valueOf(isDefaultValueExpression) : false);
-      pojo.setIsMultiselect(isMultiselect != null ? Boolean.valueOf(isMultiselect) : false);
-      pojo.setIsActive(isActive != null ? Boolean.valueOf(isActive) : true);
-      pojo.setDataType(dataType);
-      pojo.setScope(propertyScope);
-      pojo.setName(code);
-      pojo.setDisplayRows(FlexCommonUtils.isNotEmpty(displayRows) ? Integer.parseInt(displayRows.toString()) : null);
-      pojo.setDisplayColumns(FlexCommonUtils.isNotEmpty(displayColumns) ? Integer.parseInt(displayColumns.toString()) : null);
-      pojo.setListData(FlexCommonUtils.isNotEmpty(listData) ? Arrays.asList(listData.toString().trim().split(",")) : null);
-      pojo.setSubDataType(FlexCommonUtils.isNotEmpty(subDataType) ? subDataType.toString() : null);
-      pojo.setDisplayName(FlexCommonUtils.isNotEmpty(displayName) ? displayName.toString() : null);
-      pojo.setDescription(FlexCommonUtils.isNotEmpty(description) ? description.toString() : null);
-      pojo.setDefaultValue(FlexCommonUtils.isNotEmpty(defaultValue) ? defaultValue.toString() : null);
-
-      results.add(pojo);
-
-      if ("ENVINST".equals(propertyScope))
-      {
-        // will only have target property values if scope is ENVINST
-        for (int j = 0; j < numEnvironments; j++)
-        {
-          String key = code + targetEnvironmentCodes.get(j);
-          String value = tokens[j+15]; //important to add 15 here
-          codeToValue.put(key, value);
-        }
-      }
-
-      if (FlexCommonUtils.isEmpty(code))
-      {
-        errors.add("Line " + i + " is missing CODE");
-      }
-
-      if (FlexCommonUtils.isEmpty(displayName))
-      {
-        errors.add("Line " + i + " is missing DISPLAY_NAME");
-      }
-
-      if (FlexCommonUtils.isEmpty(propertyScope) || !(propertyScope.equals("ENVINST") || propertyScope.equals("PROJECT")))
-      {
-        errors.add("Line " + i + " PROPERTY_SCOPE must be ENVINST or PROJECT");
-      }
-
-      if (FlexCommonUtils.isEmpty(isRequired))
-      {
-        errors.add("Line " + i + " is missing REQUIRED");
-      }
-
-      if (FlexCommonUtils.isEmpty(dataType))
-      {
-        errors.add("Line " + i + " is missing DATA_TYPE");
-      }
-
-      if (FlexCommonUtils.isNotEmpty(subDataType) && !(subDataType.equals("DIRECTORY") || subDataType.equals("JDBCURL") || subDataType.equals("URL")))
-      {
-        errors.add("Line " + i + " SUB_DATA_TYPE must be DIRECTORY, JDBCURL or URL");
-      }
-
-      if (FlexCommonUtils.isEmpty(isEncrypted))
-      {
-        errors.add("Line " + i + " is missing ENCRYPTED");
-      }
-    }
-    LOGGER.finest("codeToValue mapping: " + codeToValue);
-
-    if (errors.size() > 0)
-    {
-      throw new FlexCheckedException(errors.toString());
-    }
-
-    LOGGER.exiting(CLZ_NAM, methodName, results.size());
-    return results;
   }
 }
