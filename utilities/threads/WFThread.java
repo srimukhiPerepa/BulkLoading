d2bd8c897ecf4bb623c03f22056d48c0effdfafc
jbrouillard <jay-ar.brouillard@flexagon.com>
2023-11-20 15:57:56 -0500

    added allowsVariant

--- utilities/threads/WFThread.java
+++ utilities/threads/WFThread.java
@@ -197,6 +197,7 @@ public class WFThread extends Thread
       pojo.setIsRequired(Boolean.valueOf(isRequired));
       pojo.setIsEncrypted(Boolean.valueOf(isEncrypted));
       pojo.setIsActive(Boolean.valueOf(isActive));
+      pojo.setIsAllowsVariant(true); // defaulting to true
 
       // optional
       if (displayName != null)
