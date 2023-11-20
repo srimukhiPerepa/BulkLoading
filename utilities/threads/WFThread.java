a05ab2d7348dc019a61a42a67bab011b09600220
jbrouillard <jay-ar.brouillard@flexagon.com>
2023-11-20 15:41:51 -0500

    searchResult.JsonObject(0).get('propertyDefinitionId').toString()

--- utilities/threads/WFThread.java
+++ utilities/threads/WFThread.java
@@ -87,7 +87,7 @@ public class WFThread extends Thread
         else
         {
           // patch
-          String propertyKeyDefinitionId = searchResult.get("propertyDefinitionId").toString();
+          String propertyKeyDefinitionId = searchResult.JsonObject(0).get("propertyDefinitionId").toString();
           JSONObject requestBody = propKeyDef.toJson();
           pAPI.patchPropertyKeyDefinitionById(propertyKeyDefinitionId, requestBody.toString());
         }
